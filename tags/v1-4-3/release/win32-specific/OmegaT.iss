8d1e25cf1d20ff0a3ab548f6fd6504b77b131739
(no author) <(no author)@b0d8beef-cb45-0410-a8e4-c0d495c3b779>
2004-09-23 14:27:31 +0000

    This commit was manufactured by cvs2svn to create tag 'v1-4-3'.
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat@64 b0d8beef-cb45-0410-a8e4-c0d495c3b779

---
+++ tags/v1-4-3/release/win32-specific/OmegaT.iss
@@ -0,0 +1,33 @@
+; -- OmegaT.iss --
+
+[Setup]
+AppName=OmegaT
+AppVerName=OmegaT version @VERSION_NUMBER_SUBST@
+DefaultDirName={pf}\OmegaT
+DefaultGroupName=OmegaT
+UninstallDisplayIcon={app}\OmegaT.exe
+Compression=lzma
+SolidCompression=yes
+LicenseFile=license.txt
+OutputDir=..\dist
+OutputBaseFilename=OmegaT_@VERSION_NUMBER_SUBST@_setup
+
+[Files]
+Source: "docs\*"; DestDir: "{app}\docs"; Flags: recursesubdirs
+Source: "images\*"; DestDir: "{app}\images"; Flags: recursesubdirs
+Source: "source\*"; DestDir: "{app}\source"; Flags: recursesubdirs
+Source: "OmegaT.bat"; DestDir: "{app}"
+Source: "OmegaT.exe"; DestDir: "{app}"
+Source: "OmegaT.jar"; DestDir: "{app}"
+Source: "license.txt"; DestDir: "{app}"
+Source: "readme.txt"; DestDir: "{app}"; Flags: isreadme
+Source: "join.html"; DestDir: "{app}"
+Source: "release_notes.txt"; DestDir: "{app}"
+
+[Icons]
+Name: "{group}\OmegaT @VERSION_NUMBER_SUBST@"; Filename: "{app}\OmegaT.exe"; WorkingDir: "{app}"
+Name: "{group}\OmegaT Readme"; Filename: "{app}\readme.txt"
+Name: "{group}\OmegaT User Manual"; Filename: "{app}\docs\index.html"
+Name: "{group}\Join OmegaT Mailing List"; Filename: "{app}\join.html"
+Name: "{group}\Uninstall OmegaT"; Filename: "{uninstallexe}"
+
---
+++ tags/v1-4-3/src/org/omegat/core/LCheckSum.java
@@ -0,0 +1,55 @@
+/**************************************************************************
+ OmegaT - Java based Computer Assisted Translation (CAT) tool
+ Copyright (C) 2002-2004  Keith Godfrey et al
+                          keithgodfrey@users.sourceforge.net
+                          907.223.2039
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+**************************************************************************/
+
+package org.omegat.core;
+
+import java.util.zip.Adler32;
+import java.util.zip.CRC32;
+
+/**
+ * Utility class to compute checksums
+ *
+ * @author Keith Godfrey
+ */
+public class LCheckSum
+{
+
+    // old method - keep it around for posterity (and because it gives
+	//	a better pseudorandom sequence)
+	public static long compute(String str)
+	{
+		CRC32 crc = new CRC32();
+		Adler32 adler = new Adler32();
+		crc.update(str.getBytes());
+		adler.update(str.getBytes());
+		long dg1 = crc.getValue();
+		long dg2 = adler.getValue();
+		crc.update(String.valueOf(dg2).getBytes());
+		adler.update(String.valueOf(dg1).getBytes());
+		long d3 = crc.getValue();
+		long d4 = adler.getValue();
+		dg1 ^= d4;
+		dg2 ^= d3;
+		return (dg2 ^ ((dg1 >>> 32) | (dg1 << 32)));
+		// convert this to first 8 bytes of SHA later
+	}
+
+}
