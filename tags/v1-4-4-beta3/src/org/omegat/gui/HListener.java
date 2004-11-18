68de371e518c6a6bc66c21370549cef4412f59ff
(no author) <(no author)@b0d8beef-cb45-0410-a8e4-c0d495c3b779>
2004-11-18 15:23:53 +0000

    This commit was manufactured by cvs2svn to create tag 'v1-4-4-beta3'.
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat@74 b0d8beef-cb45-0410-a8e4-c0d495c3b779

---
+++ tags/v1-4-4-beta3/src/org/omegat/gui/HListener.java
@@ -0,0 +1,44 @@
+/*
+ * HListener.java
+ *
+ * Created on 8 Август 2004 г., 19:48
+ */
+
+package org.omegat.gui;
+
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import org.omegat.gui.messages.MessageRelay;
+
+/**
+ * A listener for old Hyperlink-like style
+ *
+ * @author Keith Godfrey
+ */
+class HListener implements HyperlinkListener
+{
+	public HListener(TransFrame t, boolean grabFocus)
+	{
+		m_transFrame = t;
+		m_grabFocus = grabFocus;
+	}
+
+	public void hyperlinkUpdate(HyperlinkEvent e)
+	{
+		String s;
+		if (e.getEventType() == 
+		HyperlinkEvent.EventType.ACTIVATED)
+		{
+			s = e.getDescription();
+			MessageRelay.uiMessageDoGotoEntry(m_transFrame, s);
+			//m_transFrame.doGotoEntry(s);
+			if (m_grabFocus)
+			{
+				m_transFrame.toFront();
+			}
+		}
+	}
+
+	private TransFrame	m_transFrame;
+	private boolean	m_grabFocus;
+}
