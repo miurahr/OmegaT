81d31cba116dae02ceaf52d60112799fd3780f23
(no author) <(no author)@b0d8beef-cb45-0410-a8e4-c0d495c3b779>
2006-03-03 17:55:06 +0000

    This commit was manufactured by cvs2svn to create tag 'v1-6-0_RC8'.
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat@450 b0d8beef-cb45-0410-a8e4-c0d495c3b779

---
+++ tags/v1-6-0_RC8/src/org/omegat/util/PatternConsts.java
@@ -0,0 +1,73 @@
+/**************************************************************************
+ OmegaT - Computer Assisted Translation (CAT) tool 
+          with fuzzy matching, translation memory, keyword search, 
+          glossaries, and translation leveraging into updated projects.
+
+ Copyright (C) 2000-2006 Keith Godfrey and Maxym Mykhalchuk
+                         keithgodfrey@users.sourceforge.net
+                         907.223.2039
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+**************************************************************************/
+
+package org.omegat.util;
+
+import java.util.regex.Pattern;
+
+/**
+ * Constant patterns, used in dirrerent other classes.
+ *
+ * @author Maxym Mykhalchuk
+ */
+public class PatternConsts
+{
+	/** compiled pattern to extract the encoding from XML file, if any */
+	public static final Pattern XML_ENCODING = Pattern.compile(
+		"<\\?xml.*?encoding\\s*=\\s*\"(\\S+?)\".*?\\?>");                       // NOI18N
+	
+	/** compiled pattern to extract the encoding from XML file, if any */
+	public static final Pattern XML_HEADER = Pattern.compile(
+		"(<\\?xml.*?\\?>)");                                                    // NOI18N
+	
+	/** compiled pattern to extract the encoding from HTML file, if any */
+	public static final Pattern HTML_ENCODING = Pattern.compile(
+		"<meta.*?content\\s*=\\s*[\"']\\s*text/html\\s*;\\s*charset\\s*=\\s*(\\S+?)[\"'].*?/?\\s*>",  // NOI18N
+		Pattern.CASE_INSENSITIVE);
+	
+	/** compiled pattern to look for HTML file HEAD declaration */
+	public static final Pattern HTML_HEAD = Pattern.compile(
+		"<head.*?>",                                                            // NOI18N
+		Pattern.CASE_INSENSITIVE);
+	
+	/** compiled pattern to look for HTML file HTML declaration */
+	public static final Pattern HTML_HTML = Pattern.compile(
+		"<html.*?>",                                                            // NOI18N
+		Pattern.CASE_INSENSITIVE);
+    
+    /** 
+     * Pattern that matches full string containing in full and only 
+     * omegat-specific tag (without leading &lt; and trailing &gt;).
+     */
+    public static final Pattern OMEGAT_TAG_ONLY = Pattern.compile(
+            "^\\/?[a-zA-Z]+[0-9]+\\/?$");                                           // NOI18N
+
+    /** 
+     * Pattern that matches omegat-specific tags 
+     * (with leading &lt; and trailing &gt; in any place of a string).
+     */
+    public static final Pattern OMEGAT_TAG = Pattern.compile(
+            "<\\/?[a-zA-Z]+[0-9]+\\/?>");                                           // NOI18N
+    
+}
