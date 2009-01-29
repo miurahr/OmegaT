7b7ebc537b3122abbbd07c88895ed853f77ae749
alex73 <alex73@b0d8beef-cb45-0410-a8e4-c0d495c3b779>
2009-01-29 06:49:19 +0000

    Fix antialiasing support for segment's marks
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat@1973 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- branches/prototypes/new-editor/src/org/omegat/gui/editor/ViewSegmentMark.java
+++ branches/prototypes/new-editor/src/org/omegat/gui/editor/ViewSegmentMark.java
@@ -24,20 +24,8 @@
 
 package org.omegat.gui.editor;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
-import java.awt.Rectangle;
-import java.awt.Shape;
-
-import javax.swing.text.AttributeSet;
-import javax.swing.text.BadLocationException;
 import javax.swing.text.Element;
-import javax.swing.text.StyleConstants;
-import javax.swing.text.StyledDocument;
-import javax.swing.text.View;
-import javax.swing.text.Position.Bias;
+import javax.swing.text.LabelView;
 
 /**
  * Class for display segmentation marks. It better to paint marks by own
@@ -45,10 +33,7 @@ import javax.swing.text.Position.Bias;
  * 
  * @author Alex Buloichik (alex73mail@gmail.com)
  */
-public class ViewSegmentMark extends View {
-    private Font font;
-    private FontMetrics fontMetrics;
-    private Color fg, bg;
+public class ViewSegmentMark extends LabelView {
     private final boolean beginMark;
 
     public ViewSegmentMark(Element elem) {
@@ -61,149 +46,11 @@ public class ViewSegmentMark extends View {
     }
 
     /**
-     * Calculate alignment on the line.
+     * Do not break view by several parts, because we will not be able to align
+     * it in RTL presentation.
      */
     @Override
-    public float getAlignment(int axis) {
-        updateDrawSettings();
-        switch (axis) {
-        case View.X_AXIS:
-            return 0;// super.getAlignment(axis);
-        case View.Y_AXIS:
-            float h = fontMetrics.getHeight();
-            float d = fontMetrics.getDescent();
-            return (h > 0) ? (h - d) / h : 0;
-        default:
-            throw new IllegalArgumentException("Invalid axis: " + axis);
-        }
-    }
-
-    /**
-     * Calculate view size.
-     */
-    @Override
-    public float getPreferredSpan(int axis) {
-        updateDrawSettings();
-        switch (axis) {
-        case View.X_AXIS:
-            return fontMetrics.stringWidth(getText());
-        case View.Y_AXIS:
-            return fontMetrics.getHeight();
-        default:
-            throw new IllegalArgumentException("Invalid axis: " + axis);
-        }
-    }
-
-    /**
-     * Calculate where position should be displayed. Used for display caret.
-     */
-    @Override
-    public Shape modelToView(int pos, Shape a, Bias b)
-            throws BadLocationException {
-        int p0 = getElement().getStartOffset();
-
-        if (pos == p0) {
-            /*
-             * At the begin of "end mark". It can be RTL+empty segment. In this
-             * case we need to move caret between "start" and "end" marks, i.e.
-             * at the end of "end mark".
-             */
-
-        }
-
-        Rectangle alloc = (a instanceof Rectangle) ? (Rectangle) a : a
-                .getBounds();
-
-        Rectangle lineArea = new Rectangle();
-
-        String text = getText();
-        // fill in the results and return
-        lineArea.x = alloc.x
-                + fontMetrics.stringWidth(text.substring(0, pos - p0));
-        lineArea.y = alloc.y;
-        lineArea.width = 0;
-        lineArea.height = fontMetrics.getHeight();
-        return lineArea;
-    }
-
-    /**
-     * Calculate what position should be displayed at point. Used for position
-     * caret after mouse click.
-     */
-    @Override
-    public int viewToModel(float x, float y, Shape a, Bias[] biasReturn) {
-        String text = getText();
-        int p0 = getElement().getStartOffset();
-
-        float prev = 0;
-        float beg = x - a.getBounds().x;
-        for (int i = 0; i <= text.length(); i++) {
-            float pos = fontMetrics.stringWidth(text.substring(0, i)) - beg;
-            if (pos > 0) {
-                return pos < -prev ? i + p0 : i - 1 + p0;
-            }
-            prev = pos;
-        }
-        return p0 + text.length();
-    }
-
-    /**
-     * Paint this view.
-     */
-    @Override
-    public void paint(Graphics g, Shape a) {
-        updateDrawSettings();
-
-        Rectangle alloc = (a instanceof Rectangle) ? (Rectangle) a : a
-                .getBounds();
-
-        if (bg != null) {
-            g.setColor(bg);
-            g.fillRect(alloc.x, alloc.y, alloc.width, alloc.height);
-        }
-
-        g.setFont(font);
-        g.setColor(fg);
-        String text = getText();
-
-        int y = alloc.getBounds().y + fontMetrics.getHeight()
-                - fontMetrics.getDescent();
-
-        g.drawChars(text.toCharArray(), 0, text.length(), alloc.getBounds().x,
-                y);
-    }
-
-    /**
-     * Get view's text from document.
-     * 
-     * @return text
-     */
-    private String getText() {
-        int p0 = getElement().getStartOffset();
-        int p1 = getElement().getEndOffset();
-        try {
-            return getElement().getDocument().getText(p0, p1 - p0);
-        } catch (BadLocationException ex) {
-            throw new RuntimeException("Invalid location", ex);
-        }
-    }
-
-    /**
-     * Update view draw settings.
-     */
-    private void updateDrawSettings() {
-        if (font == null) {
-            AttributeSet attrs = getElement().getAttributes();
-            StyledDocument doc = (StyledDocument) getDocument();
-            font = doc.getFont(attrs);
-            fontMetrics = getContainer().getFontMetrics(font);
-
-            fg = doc.getForeground(attrs);
-            if (attrs.isDefined(StyleConstants.Background)) {
-                bg = doc.getBackground(attrs);
-            } else {
-                bg = null;
-            }
-        }
+    public int getBreakWeight(int axis, float pos, float len) {
+        return BadBreakWeight;
     }
 }
