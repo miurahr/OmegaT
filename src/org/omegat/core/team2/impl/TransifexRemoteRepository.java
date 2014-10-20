de0a904dbee1371d2d58140148c4559f0d497024
Alex Buloichik <alex73mail@gmail.com>
2014-10-20 21:44:30 +0300

    project_props2

---
+++ src/org/omegat/core/team2/impl/TransifexRemoteRepository.java
@@ -0,0 +1,10 @@
+package org.omegat.core.team2.impl;
+
+/**
+ * 
+ * https://www.transifex.com/api/2/project/xbmc-main/resource/pvr-dvblink/translation/be?mode=default&file
+ *
+ */
+public class TransifexRemoteRepository {
+
+}
