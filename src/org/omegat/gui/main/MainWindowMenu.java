ec0a7a072ba42d066119578447ee461451bd6d2c
Alex Buloichik <alex73mail@gmail.com>
2014-12-31 12:39:40 +0300

    Remove old team implementation

--- src/org/omegat/gui/main/MainWindowMenu.java
+++ src/org/omegat/gui/main/MainWindowMenu.java
@@ -252,7 +252,6 @@ public class MainWindowMenu implements ActionListener, MenuListener, IMainMenu {
         mainMenu.add(helpMenu = createMenu("TF_MENU_HELP"));
 
         projectMenu.add(projectNewMenuItem = createMenuItem("TF_MENU_FILE_CREATE"));
-        projectMenu.add(projectTeamNewMenuItem = createMenuItem("TF_MENU_FILE_TEAM_CREATE"));
         projectMenu.add(projectOpenMenuItem = createMenuItem("TF_MENU_FILE_OPEN"));
         projectMenu.add(projectOpenRecentMenuItem = createMenu("TF_MENU_FILE_OPEN_RECENT"));
 
@@ -594,7 +593,7 @@ public class MainWindowMenu implements ActionListener, MenuListener, IMainMenu {
      *            project open status: true if opened, false if closed
      */
     private void onProjectStatusChanged(final boolean isProjectOpened) {
-        JMenuItem[] itemsToSwitchOff = new JMenuItem[] { projectNewMenuItem, projectTeamNewMenuItem, projectOpenMenuItem };
+        JMenuItem[] itemsToSwitchOff = new JMenuItem[] { projectNewMenuItem, projectOpenMenuItem };
 
         JMenuItem[] itemsToSwitchOn = new JMenuItem[] { projectImportMenuItem, projectWikiImportMenuItem,
                 projectReloadMenuItem, projectCloseMenuItem, projectSaveMenuItem, projectEditMenuItem,
@@ -620,9 +619,6 @@ public class MainWindowMenu implements ActionListener, MenuListener, IMainMenu {
         }
         for (JMenuItem item : itemsToSwitchOn) {
             item.setEnabled(isProjectOpened);
-        }
-        if (Core.getParams().containsKey("no-team")) {
-        	projectTeamNewMenuItem.setEnabled(false);
         }
     }
 
@@ -731,7 +727,6 @@ public class MainWindowMenu implements ActionListener, MenuListener, IMainMenu {
     JMenuItem projectImportMenuItem;
     JMenu projectMenu;
     JMenuItem projectNewMenuItem;
-    JMenuItem projectTeamNewMenuItem;
     JMenuItem projectOpenMenuItem;
     JMenu projectOpenRecentMenuItem;
     JMenuItem projectReloadMenuItem;
