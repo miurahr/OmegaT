2394f1ca4cd5bd102dc1b2bd98d8ac2d86c3508a
Alex Buloichik <alex73mail@gmail.com>
2015-02-25 09:42:14 +0300

    Remove old team implementation

--- test/src/org/omegat/languagetools/FalseFriendsTest.java
+++ test/src/org/omegat/languagetools/FalseFriendsTest.java
@@ -39,10 +39,8 @@ import org.omegat.core.data.ProjectProperties;
 import org.omegat.core.data.ProjectTMX;
 import org.omegat.core.data.SourceTextEntry;
 import org.omegat.core.data.TMXEntry;
-import org.omegat.core.data.TMXEntry.ExternalLinked;
 import org.omegat.core.events.IProjectEventListener.PROJECT_CHANGE_TYPE;
 import org.omegat.core.statistics.StatisticsInfo;
-import org.omegat.core.team.IRemoteRepository;
 import org.omegat.gui.editor.mark.Mark;
 import org.omegat.tokenizer.ITokenizer;
 import org.omegat.util.Language;
@@ -120,10 +118,6 @@ public class FalseFriendsTest extends TestCore {
             }
 
             public ITokenizer getSourceTokenizer() {
-                return null;
-            }
-
-            public IRemoteRepository getRepository() {
                 return null;
             }
 
