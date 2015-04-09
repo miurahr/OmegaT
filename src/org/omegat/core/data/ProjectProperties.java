9064d101f2e5778adf13402616d75df1a738eae8
Alex Buloichik <alex73mail@gmail.com>
2015-04-09 09:14:45 +0300

    Fix directory mission error for writable glossary file

--- src/org/omegat/core/data/ProjectProperties.java
+++ src/org/omegat/core/data/ProjectProperties.java
@@ -158,7 +158,9 @@ public class ProjectProperties {
 
     /** Returns The Glossary File Directory */
     public String getWriteableGlossaryDir() {
-        return writableGlossaryFile.getAsFile().getParent();
+        ProjectPath dir = new ProjectPath(true);
+        dir.setRelativeOrAbsolute(writableGlossaryFile.getAsFile().getParent());
+        return dir.getAsString();
     }
 
     /** Sets The Writeable Glossary File Location */
@@ -467,9 +469,6 @@ public class ProjectProperties {
             throw new ProjectException(StaticUtils.format(OStrings.getString("PROJECT_GLOSSARY_FOLDER"), glsDir));
         }
         String wGlsDir = getWriteableGlossaryDir();
-        if (!wGlsDir.endsWith(File.separator)) {
-            wGlsDir += '/';
-        }
         if (!wGlsDir.contains(getGlossaryRoot())) {
             throw new ProjectException(StaticUtils.format(OStrings.getString("PROJECT_W_GLOSSARY"), glsDir));
         }
