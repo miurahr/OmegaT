25b9ee7c056ba7d786563f11cdc654edadc5bfc7
Alex Buloichik <alex73mail@gmail.com>
2015-04-10 18:11:18 +0300

    Autocreate folders for team project, better handle new team project(i.e. without project_save.tmx)

--- src/org/omegat/core/data/ProjectProperties.java
+++ src/org/omegat/core/data/ProjectProperties.java
@@ -401,6 +401,10 @@ public class ProjectProperties {
         this.removeTags = removeTags;
     }
 
+    public boolean hasRepositories() {
+        return repositories != null && !repositories.isEmpty();
+    }
+
     public List<RepositoryDefinition> getRepositories() {
         return repositories;
     }
@@ -490,6 +494,15 @@ public class ProjectProperties {
                 dict.mkdirs();
             }
         }
+    }
+
+    public void autocreateDirectories() {
+        getProjectInternalDir().mkdirs();
+        sourceDir.getAsFile().mkdirs();
+        targetDir.getAsFile().mkdirs();
+        glossaryDir.getAsFile().mkdirs();
+        tmDir.getAsFile().mkdirs();
+        dictDir.getAsFile().mkdirs();
     }
 
     /**
