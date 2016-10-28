2611a864314e6710a4b357179000f3dabc3444d5
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-10-29 00:19:16 +0900

    Minor cleanup in IssuesPanelController

--- src/org/omegat/gui/issues/IssuesPanelController.java
+++ src/org/omegat/gui/issues/IssuesPanelController.java
@@ -673,7 +673,7 @@ public class IssuesPanelController implements IIssues {
         }
 
         static IssueColumn get(int index) {
-            return IssueColumn.values()[index];
+            return values()[index];
         }
     }
 
