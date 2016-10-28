8d00fb7768cc0667174388325f70107230471eac
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-10-28 18:05:30 +0900

    TODO: Implement reload, restart prompts

--- src/org/omegat/gui/preferences/PreferencesView.java
+++ src/org/omegat/gui/preferences/PreferencesView.java
@@ -2,6 +2,15 @@ package org.omegat.gui.preferences;
 
 import javax.swing.JComponent;
 
+/**
+ * An interface implemented by views shown in the Preferences window.
+ * 
+ * TODO: Add a facility for indicating when the project needs to be reloaded or
+ * the program restarted
+ * 
+ * @author amake
+ *
+ */
 public interface PreferencesView {
     JComponent getGui();
 
