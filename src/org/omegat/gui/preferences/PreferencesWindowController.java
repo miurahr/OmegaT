e39a1db120bd8a2c106fe1000e016b5eeacc9438
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-10-28 16:20:15 +0900

    Add LanguageTool dialog to consolidated prefs window

--- src/org/omegat/gui/preferences/PreferencesWindowController.java
+++ src/org/omegat/gui/preferences/PreferencesWindowController.java
@@ -16,6 +16,7 @@ import javax.swing.tree.TreeSelectionModel;
 
 import org.omegat.core.Core;
 import org.omegat.core.data.NotLoadedProject;
+import org.omegat.gui.preferences.view.LanguageToolConfigurationView;
 import org.omegat.gui.preferences.view.SaveOptionsView;
 import org.omegat.gui.preferences.view.SpellcheckerConfigurationView;
 import org.omegat.gui.preferences.view.TagProcessingOptionsView;
@@ -89,6 +90,7 @@ public class PreferencesWindowController {
     public TreeNode getRootNode() {
         DefaultMutableTreeNode root = new DefaultMutableTreeNode();
         root.add(new DefaultMutableTreeNode(new SpellcheckerConfigurationView()));
+        root.add(new DefaultMutableTreeNode(new LanguageToolConfigurationView()));
         root.add(new DefaultMutableTreeNode(new WorkflowOptionsView()));
         root.add(new DefaultMutableTreeNode(new TagProcessingOptionsView()));
         root.add(new DefaultMutableTreeNode(new TeamOptionsView()));
